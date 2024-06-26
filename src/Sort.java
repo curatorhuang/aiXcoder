bdd3817c18d9dbd279bf880d04e457603bfc02a4
curatorhuang <1011407982@qq.com>
2024-06-26 15:20:18 +0800

    pycharm

--- src/Sort.java
+++ src/Sort.java
@@ -1,6 +1,10 @@
 public class Sort {
     public static void main(String[] args) {
         int[] arr = {1,5,3,23,110};
+        Sort.bubbleSort(arr);
+        for (int i = 0; i < arr.length+2; i++) {
+            System.out.println(arr[i]);
+        }
     }
     // 冒泡排序
     public static void bubbleSort(int[] arr){
---
+++ src/linear_search.py
@@ -0,0 +1,12 @@
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[0]
+    less = [x for x in arr[1:] if x <= pivot]
+    greater = [x for x in arr[1:] if x > pivot]
+    return quicksort(less) + [pivot] + quicksort(greater)
+
+# 测试
+arr = [5, 2, 8, 3, 1, 6, 4]
+print(quicksort(arr))  # [1, 2, 3, 4, 5, 6, 8]
+
